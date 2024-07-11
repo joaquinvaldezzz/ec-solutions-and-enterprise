5fe8207f91a3d043c895dd8c475dec98bafb3ef7
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-07-11 22:36:45 +0800

    chore: import Untitled UI theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from 'tailwindcss'
 
+import { blurs, boxShadows, colors } from './theme'
+
 const config: Config = {
   content: [
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,6 +9,34 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+      },
+    },
+    blur: {
+      ...blurs,
+    },
+    boxShadow: {
+      ...boxShadows,
+    },
+    colors: {
+      ...colors,
+    },
+    fontSize: {
+      xs: ['0.75rem', { lineHeight: '1.125rem' }],
+      sm: ['0.875rem', { lineHeight: '1.25rem' }],
+      md: ['1rem', { lineHeight: '1.5rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.25rem', { lineHeight: '1.875rem' }],
+      'display-xs': ['1.5rem', { lineHeight: '2rem' }],
+      'display-sm': ['1.875rem', { lineHeight: '2.375rem' }],
+      'display-md': ['2.25rem', { lineHeight: '2.75rem' }],
+      'display-lg': ['3rem', { lineHeight: '3.75rem' }],
+      'display-xl': ['3.75rem', { lineHeight: '4.5rem' }],
+      'display-2xl': ['4.5rem', { lineHeight: '5.625rem' }],
+    },
     extend: {},
   },
   plugins: [],
---
+++ theme/index.ts
@@ -0,0 +1,5 @@
+import blurs from './blurs'
+import boxShadows from './boxShadows'
+import colors from './colors'
+
+export { blurs, boxShadows, colors }
