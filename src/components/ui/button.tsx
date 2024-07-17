09258d788e1db3b9762d268a8a1e4a9a91207438
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-07-17 21:08:33 +0800

    chore: put 'use client' at the top

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { forwardRef, type ButtonHTMLAttributes } from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
@@ -5,7 +7,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-lg border font-semibold shadow-xs transition focus:outline-none focus:ring-4',
+  'relative inline-flex items-center justify-center rounded-lg border font-semibold shadow-xs transition focus:outline-none focus:ring-4',
   {
     variants: {
       size: {
