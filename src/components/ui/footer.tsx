486e5d46b10562a32e8f73e3189e7ec279cfb0aa
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-07-18 23:27:46 +0800

    feat: make footer component responsive

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -45,22 +45,27 @@ export function Footer() {
   return (
     <footer className="bg-brand-950">
       <Container>
-        <div className="py-12">
-          <div>
+        <div className="grid py-12 lg:grid-cols-3 lg:gap-16 lg:pt-16">
+          <div className="">
             <h2 className="font-semibold text-gray-200">EC Solutions</h2>
             <p className="mt-6 text-gray-200">
               Design amazing digital experiences that create more happy in the world.
             </p>
           </div>
 
-          <div className="mt-12 grid grid-cols-2 gap-8">
+          <div className="mt-12 grid grid-cols-2 gap-8 sm:grid-cols-3 md:grid-cols-5 lg:col-span-2 lg:mt-0">
             <div>
               <h3 className="text-sm font-semibold text-gray-300">Product</h3>
               <nav className="mt-4">
                 <ul className="flex flex-col gap-3">
                   {products.map((product, index) => (
                     <li key={index}>
-                      <Button className="text-gray-200" size="lg" heirarchy="link-gray" asChild>
+                      <Button
+                        className="text-gray-200 hover:text-gray-300"
+                        size="lg"
+                        heirarchy="link-gray"
+                        asChild
+                      >
                         <Link href={product.url}>{product.title}</Link>
                       </Button>
                     </li>
@@ -75,7 +80,12 @@ export function Footer() {
                 <ul className="flex flex-col gap-3">
                   {products.map((product, index) => (
                     <li key={index}>
-                      <Button className="text-gray-200" size="lg" heirarchy="link-gray" asChild>
+                      <Button
+                        className="text-gray-200 hover:text-gray-300"
+                        size="lg"
+                        heirarchy="link-gray"
+                        asChild
+                      >
                         <Link href={product.url}>{product.title}</Link>
                       </Button>
                     </li>
@@ -90,7 +100,12 @@ export function Footer() {
                 <ul className="flex flex-col gap-3">
                   {products.map((product, index) => (
                     <li key={index}>
-                      <Button className="text-gray-200" size="lg" heirarchy="link-gray" asChild>
+                      <Button
+                        className="text-gray-200 hover:text-gray-300"
+                        size="lg"
+                        heirarchy="link-gray"
+                        asChild
+                      >
                         <Link href={product.url}>{product.title}</Link>
                       </Button>
                     </li>
@@ -105,7 +120,12 @@ export function Footer() {
                 <ul className="flex flex-col gap-3">
                   {products.map((product, index) => (
                     <li key={index}>
-                      <Button className="text-gray-200" size="lg" heirarchy="link-gray" asChild>
+                      <Button
+                        className="text-gray-200 hover:text-gray-300"
+                        size="lg"
+                        heirarchy="link-gray"
+                        asChild
+                      >
                         <Link href={product.url}>{product.title}</Link>
                       </Button>
                     </li>
@@ -120,7 +140,12 @@ export function Footer() {
                 <ul className="flex flex-col gap-3">
                   {products.map((product, index) => (
                     <li key={index}>
-                      <Button className="text-gray-200" size="lg" heirarchy="link-gray" asChild>
+                      <Button
+                        className="text-gray-200 hover:text-gray-300"
+                        size="lg"
+                        heirarchy="link-gray"
+                        asChild
+                      >
                         <Link href={product.url}>{product.title}</Link>
                       </Button>
                     </li>
@@ -131,8 +156,8 @@ export function Footer() {
           </div>
         </div>
 
-        <div className="py-10">
-          <div className="flex items-center gap-6">
+        <div className="py-10 lg:flex lg:items-center lg:justify-between lg:py-12">
+          <div className="flex items-center gap-6 lg:order-2">
             <FontAwesomeIcon icon={faXTwitter} className="size-6 text-gray-400" />
             <FontAwesomeIcon icon={faLinkedin} className="size-6 text-gray-400" />
             <FontAwesomeIcon icon={faFacebook} className="size-6 text-gray-400" />
@@ -140,7 +165,7 @@ export function Footer() {
             <FontAwesomeIcon icon={faAngellist} className="size-6 text-gray-400" />
             <FontAwesomeIcon icon={faDribbble} className="size-6 text-gray-400" />
           </div>
-          <p className="mt-6 text-gray-300">
+          <p className="mt-6 text-gray-300 lg:order-1 lg:mt-0">
             &copy; 2024 EC Solutions and Enterprise. All rights reserved.
           </p>
         </div>
