41644e4975d8fd0c837c14ad170bfa5ee30aaac5
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-07-19 22:22:37 +0800

    feat: add desktop nav links

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,19 +1,57 @@
 'use client'
 
+import Link from 'next/link'
 import { Bars3Icon } from '@heroicons/react/24/outline'
+
+import { type NavItem } from '@/types/nav'
 
 import { Button } from './button'
 
+const links: NavItem[] = [
+  {
+    url: '#',
+    title: 'About Us',
+  },
+  {
+    url: '#',
+    title: 'Our Services',
+  },
+  {
+    url: '#',
+    title: 'Our Clients',
+  },
+  {
+    url: '#',
+    title: 'Contact Us',
+  },
+]
+
 export function Header() {
   return (
-    <header className="flex h-18 items-center">
-      <div className="container flex items-center justify-between">
-        <div></div>
+    <header className="absolute inset-x-0 top-0 z-50 flex h-[--header-height] items-center">
+      <div className="container flex items-center justify-between lg:grid lg:grid-cols-[theme(spacing.48)_auto_theme(spacing.48)]">
+        <div>
+          <Button size="lg" heirarchy="link-color" asChild>
+            <Link href="#">EC Solutions</Link>
+          </Button>
+        </div>
 
-        <Button className="-mr-1 size-10" heirarchy="link-gray" type="button">
+        <Button className="-mr-1 size-10 lg:hidden" heirarchy="link-gray" type="button">
           <span className="sr-only">Open mobile menu</span>
           <Bars3Icon className="size-6" />
         </Button>
+
+        <nav className="flex items-center gap-8">
+          {links.map((link, index) => (
+            <Button size="lg" heirarchy="link-gray" asChild key={index}>
+              <Link href={link.url}>{link.title}</Link>
+            </Button>
+          ))}
+        </nav>
+
+        <div className="lg:flex lg:justify-end">
+          <Button size="lg">Talk to an Expert</Button>
+        </div>
       </div>
     </header>
   )
