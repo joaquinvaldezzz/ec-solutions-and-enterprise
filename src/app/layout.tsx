faf2ce334984f98078bd10fc13f274e71d9c1a13
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-07-20 14:54:00 +0800

    feat: add basic metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,23 @@ import localFont from 'next/font/local'
 import { Footer } from '@/components/ui/footer'
 import { Header } from '@/components/ui/header'
 
+import OGImage from '@/public/images/ec-solutions-and-enterprise.jpg'
+
 import '@/styles/main.css'
 
 export const metadata: Metadata = {
   title: 'EC Solutions and Enterprise',
+  description:
+    'EC Solutions and Enterprise is a software development company that specializes in web development, mobile development, and custom software development.',
+  metadataBase: new URL('https://ecsae.vercel.app'),
+  openGraph: {
+    title: 'EC Solutions and Enterprise',
+    description:
+      'EC Solutions and Enterprise is a software development company that specializes in web development, mobile development, and custom software development.',
+    siteName: 'EC Solutions and Enterprise',
+    locale: 'en_US',
+    images: [{ url: OGImage.src, type: 'image/png', width: OGImage.width, height: OGImage.height }],
+  },
 }
 
 const inter = localFont({
