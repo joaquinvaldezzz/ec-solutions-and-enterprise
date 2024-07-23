d7e012ba51e2090a7e06aa67ff96edc46e04c2a9
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-07-23 21:30:21 +0800

    feat: implement carousel

--- src/components/block/testimonials.tsx
+++ src/components/block/testimonials.tsx
@@ -1,21 +1,20 @@
 'use client'
 
-import Image from 'next/image'
+import Image, { type StaticImageData } from 'next/image'
 
-// TODO: Implement Carousel
-// import {
-//   Carousel,
-//   CarouselContent,
-//   CarouselItem,
-//   CarouselNext,
-//   CarouselPrevious,
-// } from '../ui/carousel'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '../ui/carousel'
 import { Container, Section } from '../ui/container'
 
 import GlobalBank from '@/public/images/logos/globalbank.svg'
 import JoaquinValdez from '@/public/images/profiles-pictures/joaquin-valdez.jpg'
 
-/* interface Testimonial {
+interface Testimonial {
   quote: string
   name: string
   role: string
@@ -37,7 +36,7 @@ const testimonials: Testimonial[] = [
     role: 'UX Designer, GlobalBank',
     picture: JoaquinValdez,
   },
-] */
+]
 
 export function Testimonials() {
   return (
@@ -49,41 +48,36 @@ export function Testimonials() {
           <div className="h-px w-16 border-t" />
         </h2>
 
-        <figure className="mt-8 text-center">
-          <blockquote className="text-balance text-display-sm font-medium lg:text-display-lg lg:tracking-tight">
-            <p>
-              Untitled has saved us thousands of hours of work. We&apos;re able to spin up projects
-              and features faster.
-            </p>
-          </blockquote>
-
-          <figcaption className="mt-8 flex flex-col items-center">
-            <Image
-              className="size-16 rounded-full border border-black/10"
-              src={JoaquinValdez}
-              alt="John Joaquin Valdez"
-            />
-            <div className="mt-4 text-lg font-semibold">John Joaquin Valdez</div>
-            <div className="mt-1 text-gray-600">UX Designer, GlobalBank</div>
-          </figcaption>
-
-          <div className="mt-10 flex h-10 justify-center lg:mt-12">
-            <GlobalBank />
-          </div>
-        </figure>
-
-        {/* <Carousel>
+        <Carousel className="mt-8">
           <CarouselContent>
             {testimonials.map((testimonial, index) => (
               <CarouselItem key={index}>
+                <figure className="text-center">
+                  <blockquote className="text-balance text-display-sm font-medium lg:text-display-lg lg:tracking-tight">
+                    <p>{testimonial.quote}</p>
+                  </blockquote>
 
+                  <figcaption className="mt-8 flex flex-col items-center">
+                    <Image
+                      className="size-16 rounded-full border border-black/10"
+                      src={testimonial.picture}
+                      alt={testimonial.name}
+                    />
+                    <div className="mt-4 text-lg font-semibold">{testimonial.name}</div>
+                    <div className="mt-1 text-gray-600">{testimonial.role}</div>
+                  </figcaption>
+
+                  <div className="mt-10 flex h-10 justify-center lg:mt-12">
+                    <GlobalBank />
+                  </div>
+                </figure>
               </CarouselItem>
             ))}
           </CarouselContent>
 
           <CarouselPrevious />
           <CarouselNext />
-        </Carousel> */}
+        </Carousel>
       </Container>
     </Section>
   )
