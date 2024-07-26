a9df522441c4c5b4b17570a486bb26afdf03f717
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-07-26 22:11:07 +0800

    refactor: export slugify function

--- src/components/ui/mdx.tsx
+++ src/components/ui/mdx.tsx
@@ -1,24 +1,17 @@
 import { type MDXComponents } from 'mdx/types'
 import { MDXRemote, type MDXRemoteProps } from 'next-mdx-remote/rsc'
 
-function slugify(text: string) {
-  return text
-    .toString() // Convert to string
-    .toLowerCase() // Change to lowercase
-    .trim() // Remove whitespace from both ends of a string
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w-]+/g, '') // Remove all non-word characters except for -
-    .replace(/--+/g, '-') // Replace multiple - with single -
-}
-
 const components: MDXComponents = {
   h1: (props) => (
-    <h1 className="mb-4 text-display-sm font-semibold lg:text-display-sm">
-      <a href={`#${slugify(String(props.children))}`}>{props.children}</a>
-    </h1>
+    <h1
+      className="my-4 text-balance text-display-sm font-semibold first:mt-0 lg:text-display-sm"
+      {...props}
+    />
   ),
-  p: (props) => <p className="my-4 text-gray-600 lg:text-lg" {...props} />,
+  h2: (props) => (
+    <h2 className="mt-8 text-balance text-xl font-semibold lg:text-display-xs" {...props} />
+  ),
+  p: (props) => <p className="my-4 text-gray-600 first:mt-0 last:mb-0 lg:text-lg" {...props} />,
 }
 
 export function CustomMDX(props: MDXRemoteProps) {
