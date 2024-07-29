a4adaf1e3012f9dcc078d274e0066ed0ec2fbdfa
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-07-29 17:45:39 +0800

    feat: update MDX components

--- src/components/ui/mdx.tsx
+++ src/components/ui/mdx.tsx
@@ -4,21 +4,24 @@ import { MDXRemote, type MDXRemoteProps } from 'next-mdx-remote/rsc'
 const components: MDXComponents = {
   h1: (props) => (
     <h1
-      className="mb-4 text-balance text-display-sm font-semibold not-first:mt-4 lg:text-display-sm"
+      className="mb-4 text-balance text-display-sm font-semibold lg:mb-5 lg:text-display-sm"
       {...props}
     />
   ),
   h2: (props) => (
-    <h2 className="mb-4 mt-8 text-balance text-xl font-semibold lg:text-display-xs" {...props} />
+    <h2
+      className="mb-3 mt-8 text-balance text-xl font-semibold first:mt-0 lg:mb-4 lg:text-display-xs"
+      {...props}
+    />
   ),
   p: (props) => (
-    <p className="mt-4 text-gray-600 first:mt-0 last:mb-0 not-last:mb-4 lg:text-lg" {...props} />
+    <p className="my-4 text-gray-600 first:mt-0 last:mb-0 lg:my-4.5 lg:text-lg" {...props} />
   ),
 
   img: (props) => (
     // eslint-disable-next-line @next/next/no-img-element
     <img
-      className="rounded-xl not-first:mt-10 not-last:mb-10"
+      className="my-10 rounded-xl lg:my-12"
       src={props.src}
       alt={props.alt}
       decoding="async"
