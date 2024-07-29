f1907c76d25b8d2a54d7c51163950a3e835ba990
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-07-29 13:23:31 +0800

    feat: remove transition

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -27,7 +27,7 @@ const Checkbox = forwardRef<ElementRef<typeof Root>, CheckboxProps>(
   ({ className, size, ...props }, ref) => (
     <Root
       className={cn(
-        'peer shrink-0 cursor-pointer rounded border border-gray-300 transition focus:outline-none focus:ring-4 focus:ring-gray-400/15 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-300 aria-invalid:border-error-300 aria-invalid:ring-error-500/25 data-checked:border-brand-600 data-checked:bg-brand-600 data-checked:text-white',
+        'peer shrink-0 cursor-pointer rounded border border-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-400/15 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-300 aria-invalid:border-error-300 aria-invalid:ring-error-500/25 data-checked:border-brand-600 data-checked:bg-brand-600 data-checked:text-white',
         size === 'sm' ? 'size-4 rounded' : 'size-5 rounded-md',
         className,
       )}
