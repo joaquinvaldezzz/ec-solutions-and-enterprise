85b8756c3d12a1c0d04281f07d36de141d4165e2
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-08-01 00:05:19 +0800

    feat: update photo collage's placement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image, { type StaticImageData } from 'next/image'
 
 import { Container, Section } from '@/components/ui/container'
 import { ContactUs } from '@/components/block/contact-us'
 import { OurClients } from '@/components/block/our-clients'
-import { Testimonials } from '@/components/block/testimonials'
 import { WhatWeDo } from '@/components/block/what-we-do'
 import { WhoWeAre } from '@/components/block/who-we-are'
 
 import MDGridCheckBackground from '@/public/images/backgrounds/md-grid-check-bg.svg'
 import PlaceholderImage from '@/public/images/backgrounds/placeholder-image.jpeg'
 import SMGridCheckBackground from '@/public/images/backgrounds/sm-grid-check-bg.svg'
+
+const images: StaticImageData[][] = [
+  [PlaceholderImage, PlaceholderImage, PlaceholderImage],
+  [PlaceholderImage, PlaceholderImage, PlaceholderImage],
+  [PlaceholderImage, PlaceholderImage],
+  [PlaceholderImage],
+]
 
 export default function Home() {
   return (
@@ -20,143 +25,42 @@ export default function Home() {
         <MDGridCheckBackground className="absolute left-1/2 top-0 hidden h-[90rem] w-[120rem] -translate-x-1/2 lg:block" />
 
         <Container className="relative">
-          <div className="lg:flex lg:justify-center">
-            <Link
-              className="inline-flex w-max items-center gap-2 rounded-2xl bg-brand-50 py-1 pl-1 pr-2.5 ring-1 ring-inset ring-brand-200"
-              href="#"
-            >
-              <div className="rounded-full bg-white px-2 py-0.5 text-xs font-medium text-brand-700 ring-1 ring-inset ring-brand-200">
-                We&apos;re hiring!
-              </div>
-              <div className="flex items-center gap-1 text-xs font-medium text-brand-700">
-                Join our design team
-                <svg className="size-4 text-brand-500" fill="none" viewBox="0 0 16 16">
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M3.333 8h9.334m0 0L8 3.333M12.667 8 8 12.667"
-                  />
-                </svg>
-              </div>
-            </Link>
-          </div>
-
           <div className="lg:flex lg:flex-col lg:items-center lg:text-center">
-            <h1 className="mt-4 text-balance text-display-md font-medium tracking-tight text-gray-900 lg:text-display-xl lg:tracking-tight">
+            <h1 className="text-balance text-display-md font-medium capitalize tracking-tight text-gray-900 lg:text-display-xl lg:tracking-tight">
               Partner with us and experience the future of technology today
             </h1>
 
-            <p className="mt-4 max-w-120 text-lg text-gray-600 md:text-balance lg:mt-6 lg:text-xl">
-              Join the many businesses that have already transformed their operations with us.
+            <p className="mt-4 max-w-120 text-lg capitalize text-gray-600 md:text-balance lg:mt-6 lg:text-xl">
+              Become one of the many businesses transforming their operations with our expertise.
             </p>
           </div>
 
-          <div className="relative -mx-4 mt-16 h-[22.375rem] overflow-hidden pt-[2.375rem] lg:mx-0 lg:rounded-2xl">
-            <div className="absolute inset-x-0 bottom-0 h-80 w-full bg-gray-100 lg:h-120" />
-            <div className="mt-[-2.375rem] flex rotate-[30deg] flex-col gap-2.5 *:shrink-0 lg:gap-4">
-              <div className="flex gap-2.5 pl-[7.25rem] *:shrink-0 lg:gap-4">
-                <div className="relative h-28 w-[11.625rem] overflow-hidden rounded-xl lg:h-[11.875rem] lg:w-[19.75rem]">
-                  <Image
-                    className="object-cover"
-                    src={PlaceholderImage}
-                    alt=""
-                    fill
-                    priority
-                    placeholder="blur"
-                  />
+          <div className="relative -mx-4 mt-[calc(theme(spacing.16)-2.375rem)] h-[22.375rem] overflow-hidden pt-[2.375rem] md:pt-40 lg:mx-0 lg:mt-24 lg:h-120">
+            <div className="absolute inset-x-0 bottom-0 h-80 w-full bg-gray-100 lg:h-120 lg:rounded-2xl" />
+            <div className="mt-[-2.375rem] flex rotate-[30deg] flex-col gap-2.5 *:shrink-0 md:-mt-14 lg:-ml-32 lg:-mt-64 lg:gap-4">
+              {images.map((row, index) => (
+                <div
+                  className="flex gap-2.5 *:shrink-0 odd:pl-[7.25rem] even:pl-5 lg:gap-4 lg:odd:pl-[40.25rem] lg:even:pl-[30.25rem]"
+                  key={index}
+                >
+                  {row.map((src, i) => (
+                    <div
+                      className="relative h-28 w-[11.625rem] overflow-hidden rounded-xl lg:h-[11.875rem] lg:w-[19.75rem]"
+                      key={i}
+                    >
+                      <Image
+                        className="object-cover"
+                        src={src}
+                        alt=""
+                        fill
+                        sizes="(max-width: 1024px) 50vw, 100vw"
+                        priority
+                        placeholder="blur"
+                      />
+                    </div>
+                  ))}
                 </div>
-                <div className="relative h-28 w-[11.625rem] overflow-hidden rounded-xl lg:h-[11.875rem] lg:w-[19.75rem]">
-                  <Image
-                    className="object-cover"
-                    src={PlaceholderImage}
-                    alt=""
-                    fill
-                    priority
-                    placeholder="blur"
-                  />
-                </div>
-                <div className="relative h-28 w-[11.625rem] overflow-hidden rounded-xl lg:h-[11.875rem] lg:w-[19.75rem]">
-                  <Image
-                    className="object-cover"
-                    src={PlaceholderImage}
-                    alt=""
-                    fill
-                    priority
-                    placeholder="blur"
-                  />
-                </div>
-              </div>
-
-              <div className="flex gap-2.5 pl-5 *:shrink-0 lg:gap-4">
-                <div className="relative h-28 w-[11.625rem] overflow-hidden rounded-xl lg:h-[11.875rem] lg:w-[19.75rem]">
-                  <Image
-                    className="object-cover"
-                    src={PlaceholderImage}
-                    alt=""
-                    fill
-                    priority
-                    placeholder="blur"
-                  />
-                </div>
-                <div className="relative h-28 w-[11.625rem] overflow-hidden rounded-xl lg:h-[11.875rem] lg:w-[19.75rem]">
-                  <Image
-                    className="object-cover"
-                    src={PlaceholderImage}
-                    alt=""
-                    fill
-                    priority
-                    placeholder="blur"
-                  />
-                </div>
-                <div className="relative h-28 w-[11.625rem] overflow-hidden rounded-xl lg:h-[11.875rem] lg:w-[19.75rem]">
-                  <Image
-                    className="object-cover"
-                    src={PlaceholderImage}
-                    alt=""
-                    fill
-                    priority
-                    placeholder="blur"
-                  />
-                </div>
-              </div>
-
-              <div className="flex gap-2.5 pl-[7.25rem] *:shrink-0 lg:gap-4">
-                <div className="relative h-28 w-[11.625rem] overflow-hidden rounded-xl lg:h-[11.875rem] lg:w-[19.75rem]">
-                  <Image
-                    className="object-cover"
-                    src={PlaceholderImage}
-                    alt=""
-                    fill
-                    priority
-                    placeholder="blur"
-                  />
-                </div>
-                <div className="relative h-28 w-[11.625rem] overflow-hidden rounded-xl lg:h-[11.875rem] lg:w-[19.75rem]">
-                  <Image
-                    className="object-cover"
-                    src={PlaceholderImage}
-                    alt=""
-                    fill
-                    priority
-                    placeholder="blur"
-                  />
-                </div>
-              </div>
-
-              <div className="flex gap-2.5 pl-5 *:shrink-0 lg:gap-4">
-                <div className="relative h-28 w-[11.625rem] overflow-hidden rounded-xl lg:h-[11.875rem] lg:w-[19.75rem]">
-                  <Image
-                    className="object-cover"
-                    src={PlaceholderImage}
-                    alt=""
-                    fill
-                    priority
-                    placeholder="blur"
-                  />
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </Container>
@@ -165,7 +69,6 @@ export default function Home() {
       <WhoWeAre />
       <WhatWeDo />
       <OurClients />
-      <Testimonials />
       <ContactUs />
     </div>
   )
