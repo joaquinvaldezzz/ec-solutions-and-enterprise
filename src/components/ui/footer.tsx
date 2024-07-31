5ac5c2cbeb8b4a2f3145a6ddb6f5885ec376e8cd
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-07-31 23:11:02 +0800

    feat: hide footer nav links

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,8 +1,5 @@
-import Link from 'next/link'
-
 import { type NavItem } from '@/types/nav'
 
-import { Button } from './button'
 import { Container } from './container'
 
 export const products: NavItem[] = [
@@ -36,7 +33,7 @@ export function Footer() {
   return (
     <footer className="bg-brand-950">
       <Container>
-        <div className="grid py-12 lg:grid-cols-3 lg:gap-16 lg:pt-16">
+        {/* <div className="grid py-12 lg:grid-cols-3 lg:gap-16 lg:pt-16">
           <div className="">
             <h2 className="font-semibold text-gray-200">EC Solutions</h2>
             <p className="mt-6 text-gray-200">
@@ -140,10 +137,10 @@ export function Footer() {
               </nav>
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <div className="py-10 lg:flex lg:items-center lg:justify-between lg:py-12">
-          <p className="mt-6 text-gray-300 lg:order-1 lg:mt-0">
+        <div className="py-10 lg:py-12">
+          <p className="text-center text-gray-300">
             &copy; 2024 EC Solutions and Enterprise. All rights reserved.
           </p>
         </div>
