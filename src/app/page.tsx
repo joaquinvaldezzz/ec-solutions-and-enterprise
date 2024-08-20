5292795b91813e29777b62e4a777c5e6f414fa1b
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2024-08-20 17:24:20 +0800

    feat: reorder sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,8 +132,8 @@ export default function Page() {
         </Container>
       </Section>
 
-      <OurClients />
       <AboutUs />
+      <OurClients />
       <OurServices />
       <ContactUs />
     </div>
