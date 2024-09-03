da3ebcadf6e2f75dab34a34e71749682fe0171fa
John Joaquin Valdez <valdez.johnjoaquin.13579@gmail.com>
2024-09-03 17:01:53 +0800

    chore: install purgecss
    - chore: ignore spelling issues

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,17 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    '@fullhuman/postcss-purgecss': process.env.NODE_ENV === 'production' && {
+      content: ['./src/app/**/*.{ts,tsx,mdx}', './src/components/**/*.{ts,tsx,mdx}'],
+      defaultExtractor: (content) => {
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]+/g) || []
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]+/g) || []
+        return broadMatches.concat(innerMatches)
+      },
+      keyframes: true,
+      variables: true,
+    },
     autoprefixer: {},
     'postcss-sort-media-queries': {},
     tailwindcss: {},
