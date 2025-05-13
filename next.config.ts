91c334eacf6eca169e338359167a1d2c75446fb8
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-05-13 09:35:55 +0800

    fix: enhance SVG handling

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,38 @@
 import type { NextConfig } from "next";
+import type { Configuration, RuleSetRule } from "webpack";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["tsx", "md", "mdx"],
 
-  webpack: (config) => {
+  webpack: (config: Configuration) => {
     // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
+    const fileLoaderRule = config.module?.rules?.find((rule): rule is RuleSetRule => {
+      if (rule === undefined || typeof rule !== "object") return false;
+      return (
+        typeof rule?.test === "object" && rule.test instanceof RegExp && rule.test.test(".svg")
+      );
+    });
 
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: ["@svgr/webpack"],
-      },
-    );
+    if (fileLoaderRule != null && config.module?.rules != null) {
+      config.module.rules.push(
+        // Reapply the existing rule, but only for svg imports ending in ?url
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/, // *.svg?url
+        },
+        // Convert all other *.svg imports to React components
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [/url/] }, // exclude if *.svg?url
+          use: ["@svgr/webpack"],
+        },
+      );
 
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i;
+      // Modify the file loader rule to ignore *.svg, since we have it handled now.
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
 
     return config;
   },
