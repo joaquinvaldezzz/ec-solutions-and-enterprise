939066d90e9bb36a2cd8ff6c475e5ec02eaf7d65
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2026-01-11 14:20:57 +0800

    feat: bump tailwindcss to 4.1.18

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -57,7 +57,7 @@ function TableHead({ className, ...props }: ComponentProps<"th">) {
   return (
     <th
       className={cn(
-        "h-10 px-2 text-left align-middle font-medium whitespace-nowrap text-foreground [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+        "h-10 px-2 text-left align-middle font-medium whitespace-nowrap text-foreground [&:has([role=checkbox])]:pr-0 *:[[role=checkbox]]:translate-y-0.5",
         className,
       )}
       data-slot="table-head"
@@ -70,7 +70,7 @@ function TableCell({ className, ...props }: ComponentProps<"td">) {
   return (
     <td
       className={cn(
-        "p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+        "p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 *:[[role=checkbox]]:translate-y-0.5",
         className,
       )}
       data-slot="table-cell"
